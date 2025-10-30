6891463dc0578c29cd647956e29534363bd4a2d0
ludwici <gammaclone@gmail.com>
2025-10-30 19:51:05 +0200

    Added random decorations

--- src/main/java/com/ludwici/slimeoverhaul/world/structure/FireShrineStructure.java
+++ src/main/java/com/ludwici/slimeoverhaul/world/structure/FireShrineStructure.java
@@ -1,53 +1,22 @@
 package com.ludwici.slimeoverhaul.world.structure;
 
 import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.core.Holder;
-import net.minecraft.core.RegistryAccess;
-import net.minecraft.data.worldgen.VillagePools;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.*;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.BiomeSource;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.Mirror;
-import net.minecraft.world.level.block.Rotation;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkGenerator;
-import net.minecraft.world.level.levelgen.Heightmap;
-import net.minecraft.world.level.levelgen.RandomState;
-import net.minecraft.world.level.levelgen.WorldgenRandom;
 import net.minecraft.world.level.levelgen.structure.*;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceSerializationContext;
-import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceType;
-import net.minecraft.world.level.levelgen.structure.pieces.StructurePiecesBuilder;
-import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
-import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
-import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
-import net.neoforged.fml.config.IConfigSpec;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 import static com.ludwici.slimeoverhaul.Content.*;
-import static com.ludwici.slimeoverhaul.SlimeOverhaulMod.MODID;
 
 public class FireShrineStructure extends SinglePieceStructure {
-    private static final ResourceLocation FLOOR = ResourceLocation.fromNamespaceAndPath(MODID, "fire_shrine_floor");
-
-    private static final List<Block> DEEPSLATE = List.of(Blocks.DEEPSLATE_BRICKS, Blocks.CRACKED_DEEPSLATE_BRICKS);
-    private static final List<Block> DEEPSLATE_TILES = List.of(Blocks.DEEPSLATE_TILES, Blocks.CRACKED_DEEPSLATE_TILES);
-    private static final List<Block> BLACKSTONE = List.of(Blocks.POLISHED_BLACKSTONE_BRICKS, Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS);
-
-//    private static final List<List<Block>> BLOCK_SELECTOR = List.of(DEEPSLATE, DEEPSLATE_TILES, BLACKSTONE);
-
-    private static final FireShrinePiece.BlockSelector BLOCK_SELECTOR = new FireShrinePiece.BlockSelector();
+    private static final FireShrinePiece.DeepslateBricksSelector DEEPSLATE_BRICKS_SELECTOR = new FireShrinePiece.DeepslateBricksSelector();
+    private static final FireShrinePiece.DeepslateTilesSelector DEEPSLATE_TILES_SELECTOR = new FireShrinePiece.DeepslateTilesSelector();
 
     public static final MapCodec<FireShrineStructure> CODEC = simpleCodec(FireShrineStructure::new);
 
@@ -94,21 +63,25 @@ public class FireShrineStructure extends SinglePieceStructure {
                 placeBlock(worldGenLevel, Blocks.NETHERRACK.defaultBlockState(), pos.getX(), pos.getY(), pos.getZ(), boundingBox);
             }
 
-            generateBox(worldGenLevel, boundingBox, 4, 0, 3, 4, 1, 3, false, randomSource, BLOCK_SELECTOR);
-            generateBox(worldGenLevel, boundingBox, 7, 0, 3, 7, 1, 3, false, randomSource, BLOCK_SELECTOR);
-            generateBox(worldGenLevel, boundingBox, 5, 2, 3, 6, 2, 3, false, randomSource, BLOCK_SELECTOR);
+            StructurePiece.BlockSelector selector = getSelector(randomSource);
+            generateBox(worldGenLevel, boundingBox, 4, 0, 3, 4, 1, 3, false, randomSource, selector);
+            generateBox(worldGenLevel, boundingBox, 7, 0, 3, 7, 1, 3, false, randomSource, selector);
+            generateBox(worldGenLevel, boundingBox, 5, 2, 3, 6, 2, 3, false, randomSource, selector);
 
-            generateBox(worldGenLevel, boundingBox, 4, 0, 10, 4, 1, 10, false, randomSource, BLOCK_SELECTOR);
-            generateBox(worldGenLevel, boundingBox, 7, 0, 10, 7, 1, 10, false, randomSource, BLOCK_SELECTOR);
-            generateBox(worldGenLevel, boundingBox, 5, 2, 10, 6, 2, 10, false, randomSource, BLOCK_SELECTOR);
+            selector = getSelector(randomSource);
+            generateBox(worldGenLevel, boundingBox, 4, 0, 10, 4, 1, 10, false, randomSource, selector);
+            generateBox(worldGenLevel, boundingBox, 7, 0, 10, 7, 1, 10, false, randomSource, selector);
+            generateBox(worldGenLevel, boundingBox, 5, 2, 10, 6, 2, 10, false, randomSource, selector);
 
-            generateBox(worldGenLevel, boundingBox, 2, 0, 5, 2, 1, 5, false, randomSource, BLOCK_SELECTOR);
-            generateBox(worldGenLevel, boundingBox, 2, 0, 8, 2, 1, 8, false, randomSource, BLOCK_SELECTOR);
-            generateBox(worldGenLevel, boundingBox, 2, 2, 6, 2, 2, 7, false, randomSource, BLOCK_SELECTOR);
+            selector = getSelector(randomSource);
+            generateBox(worldGenLevel, boundingBox, 2, 0, 5, 2, 1, 5, false, randomSource, selector);
+            generateBox(worldGenLevel, boundingBox, 2, 0, 8, 2, 1, 8, false, randomSource, selector);
+            generateBox(worldGenLevel, boundingBox, 2, 2, 6, 2, 2, 7, false, randomSource, selector);
 
-            generateBox(worldGenLevel, boundingBox, 9, 0, 5, 9, 1, 5, false, randomSource, BLOCK_SELECTOR);
-            generateBox(worldGenLevel, boundingBox, 9, 0, 8, 9, 1, 8, false, randomSource, BLOCK_SELECTOR);
-            generateBox(worldGenLevel, boundingBox, 9, 2, 6, 9, 2, 7, false, randomSource, BLOCK_SELECTOR);
+            selector = getSelector(randomSource);
+            generateBox(worldGenLevel, boundingBox, 9, 0, 5, 9, 1, 5, false, randomSource, selector);
+            generateBox(worldGenLevel, boundingBox, 9, 0, 8, 9, 1, 8, false, randomSource, selector);
+            generateBox(worldGenLevel, boundingBox, 9, 2, 6, 9, 2, 7, false, randomSource, selector);
 
             int fireCount = 1 + randomSource.nextInt(4);
 
@@ -119,16 +92,49 @@ public class FireShrineStructure extends SinglePieceStructure {
                 BlockPos pos = shuffledPositions.get(i);
                 placeBlock(worldGenLevel, Blocks.FIRE.defaultBlockState(), pos.getX(), pos.getY() + 1, pos.getZ(), boundingBox);
             }
+
+            int count = randomSource.nextIntBetweenInclusive(0, 3);
+
+            for (int i = 0; i < count; i++) {
+                int randomX = randomSource.nextIntBetweenInclusive(3, 6);
+                int randomZ = randomSource.nextIntBetweenInclusive(5, 9);
+                int randomHeight = randomSource.nextIntBetweenInclusive(1, 3);
+
+                for (int j = 0; j < randomHeight; j++) {
+                    placeBlock(worldGenLevel, Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS.defaultBlockState(), randomX, j, randomZ, boundingBox);
+                }
+
+                placeBlock(worldGenLevel, ANCIENT_FIRE_SLIMY_BLOCK.get().defaultBlockState(), randomX, -1, randomZ, boundingBox);
+            }
+
         }
 
-        static class BlockSelector extends StructurePiece.BlockSelector {
+        private StructurePiece.BlockSelector getSelector(RandomSource randomSource) {
+            if (randomSource.nextFloat() < 0.4F) {
+                return DEEPSLATE_BRICKS_SELECTOR;
+            } else {
+                return DEEPSLATE_TILES_SELECTOR;
+            }
+        }
 
+        static class DeepslateBricksSelector extends StructurePiece.BlockSelector {
             @Override
             public void next(RandomSource randomSource, int i, int i1, int i2, boolean b) {
                 if (randomSource.nextFloat() < 0.4F) {
-                    this.next = Blocks.POLISHED_BLACKSTONE_BRICKS.defaultBlockState();
+                    this.next = Blocks.DEEPSLATE_BRICKS.defaultBlockState();
                 } else {
-                    this.next = Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS.defaultBlockState();
+                    this.next = Blocks.CRACKED_DEEPSLATE_BRICKS.defaultBlockState();
+                }
+            }
+        }
+
+        static class DeepslateTilesSelector extends StructurePiece.BlockSelector {
+            @Override
+            public void next(RandomSource randomSource, int i, int i1, int i2, boolean b) {
+                if (randomSource.nextFloat() < 0.4F) {
+                    this.next = Blocks.DEEPSLATE_TILES.defaultBlockState();
+                } else {
+                    this.next = Blocks.CRACKED_DEEPSLATE_TILES.defaultBlockState();
                 }
             }
         }
