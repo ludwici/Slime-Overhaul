113501e4ba602e590afc81df183ce3a15685d06a
ludwici <gammaclone@gmail.com>
2025-11-16 21:17:00 +0200

    Minor changes

--- src/main/java/com/ludwici/slimeoverhaul/block/slimy/AncientEarthSlimyBlock.java
+++ src/main/java/com/ludwici/slimeoverhaul/block/slimy/AncientEarthSlimyBlock.java
@@ -6,5 +6,6 @@ import net.minecraft.world.level.block.Blocks;
 public class AncientEarthSlimyBlock extends AncientSlimyBlock{
     public AncientEarthSlimyBlock(Properties properties) {
         super(Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS, SoundEvents.BRUSH_GRAVEL, SoundEvents.BRUSH_GRAVEL_COMPLETED, properties);
+        blockBehaviourList.add(new SlimyBlockBehaviour.Earth());
     }
 }
